8b9fc80b2dd1908eb14419221b570653b61e8f4d
DanielBamee <elgranchori64@gmail.com>
2025-01-21 17:57:10 +0000

    Decoraciones y colisiones añadidas

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,22 +5,26 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
     public InputAction MoveAction;
 
     // Start is called before the first frame update
     void Start()
     {
-        QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 10;
-
         MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
-        Vector2 position = (Vector2)transform.position + move * 4 * Time.deltaTime;
-        transform.position = position;
+        move = MoveAction.ReadValue<Vector2>();
     }
-} 
+
+    void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rigidbody2d.position + move * 4.0f * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+    }
+}
