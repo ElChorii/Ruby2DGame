ee1c9ad32b86b9601ed0658dcb3bfc5acdb8b73a
DanielBamee <elgranchori64@gmail.com>
2025-01-29 20:23:32 +0000

    Vida de jugador 1/2
    Vida añadida, game enviroment finalizado

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,12 +9,20 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rigidbody2d;
     Vector2 move;
     public InputAction MoveAction;
+    public int velocidadJugador = 4;
+
+    public int maxHealth = 5;
+    public int currentHealth;
+
 
     // Start is called before the first frame update
     void Start()
     {
         MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
+
     }
 
     // Update is called once per frame
@@ -24,7 +33,12 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector2 position = (Vector2)rigidbody2d.position + move * 4.0f * Time.deltaTime;
+        Vector2 position = (Vector2)rigidbody2d.position + move * velocidadJugador * Time.deltaTime;
         rigidbody2d.MovePosition(position);
+    }
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
